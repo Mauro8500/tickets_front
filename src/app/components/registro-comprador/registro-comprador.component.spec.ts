cc8c13be6df9868c3a4492a7494bb9be9a4d0dd3
vilcs <mikymogul@hotmail.com>
2022-04-03 20:35:10 -0400

    material

---
+++ src/app/components/registro-comprador/registro-comprador.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistroCompradorComponent } from './registro-comprador.component';
+
+describe('RegistroCompradorComponent', () => {
+  let component: RegistroCompradorComponent;
+  let fixture: ComponentFixture<RegistroCompradorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegistroCompradorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegistroCompradorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
