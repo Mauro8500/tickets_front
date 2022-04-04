c4c82fac4d44b322f8b39dbc9e8873ada764298f
Mauricio Illanes <mauricioillanes17@gmail.com>
2022-04-03 23:13:36 -0400

    menu2

--- src/app/components/dashboard/dashboard-routing.module.ts
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-//import { InicioComponent } from './inicio/inicio.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EmpresaComponent } from './empresa/empresa.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, children: [
+    { path: '', component: InicioComponent },
+    { path: 'eventos', component: EventosComponent },
+    { path: 'empresa', component: EmpresaComponent },
   ]}
-
 ];
 
 @NgModule({
