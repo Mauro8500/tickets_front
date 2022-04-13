eab8b57812f18ec18898201f7d65daa9155f6aab
vilcs <mikymogul@hotmail.com>
2022-04-12 22:13:20 -0400

    correccion de bugs 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component'
-import { CompradorComponent } from './components/registros/comprador/comprador.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
