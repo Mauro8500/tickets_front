b46146be31c95755db79585e66d2e49ba7f9fd50
cdmmucb <carlos.mendizabal@ucb.edu.bo>
2022-04-16 11:21:32 -0400

    integracion

--- src/app/components/dashboard/eventos/eventos.component.ts
+++ src/app/components/dashboard/eventos/eventos.component.ts
@@ -8,18 +8,35 @@ import { TicketsService } from 'src/app/tickets.service';
 })
 export class EventosComponent implements OnInit {
 
+  eventos = [
+    { nombre: 'Frank', lugar: 'Murphy', precio: 4 },
+];
   constructor(private ticketsService: TicketsService) { }
 
   ngOnInit(): void {
     this.ticketsService.getEventos().subscribe((response: any)=>{
       console.log(response);
+      this.eventos = response
     });
   }
 
   getEventosNombre(nombre: String){
     this.ticketsService.getEventosNombre(nombre).subscribe((response: any)=>{
       console.log(response);
+      this.eventos = response
     });
   }
+
+  entrarEvento(evento: any){
+      console.log("mas info");
+      console.log(evento);
+      //mandar interfaz donde sale evento con sus imagenes e info?
+  }
+
+  comprarTickets(evento: any){
+    console.log("comprar tickets");
+    console.log(evento);
+    //mandar interfaz donde sale evento con sus imagenes e info?
+}
 
 }
